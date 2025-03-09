61a6ecd52938fecaa16a9391ae61295aa05dc2b5
ntorbinskiy <n.torbinskiy@gmail.com>
2025-03-09 18:33:03 +0200

    Fix build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,21 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { inter, poppins, roboto } from "./fonts";
-import React from "react";
-import { NavBar } from "@/components/nav-bar/NavBar";
+import type { Metadata } from 'next';
+import './globals.css';
+import { inter, poppins, roboto } from './fonts';
+import React from 'react';
 
 export const metadata: Metadata = {
-    title: "Contact Us | Company Name",
-    description:
-        "Contact us for any inquiries or questions.",
+    title: 'Contact Us | Company Name',
+    description: 'Contact us for any inquiries or questions.',
 };
 
 interface RootLayoutProps {
     children: React.ReactNode;
 }
 
-export const RootLayout = async ({
-    children,
-}: RootLayoutProps): Promise<React.ReactNode> => {
+export default function RootLayout({ children }: RootLayoutProps): React.ReactNode {
     return (
-        <html
-            lang="en"
-            className={`${inter.variable} ${poppins.variable} ${roboto.variable}`}
-        >
-            <body className="font-poppins bg-gray-50">
-                <NavBar />
-                {children}
-            </body>
+        <html lang="en" className={`${inter.variable} ${poppins.variable} ${roboto.variable}`}>
+            <body className="font-poppins bg-gray-50">{children}</body>
         </html>
     );
-};
-
-export default RootLayout;
+}
